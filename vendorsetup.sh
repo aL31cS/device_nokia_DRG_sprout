302db76c5f32d7725272b0bd3ea9d0efeb79819b
RaghuVarma <raghuvarma330@gmail.com>
2019-04-05 21:34:19 +0530

    DRG: Add extra lines related to OS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,11 @@
 for i in eng userdebug user; do
 	add_lunch_combo aosp_DRG-${i}
+	add_lunch_combo dot_DRG-${i}
+	add_lunch_combo havoc_DRG-${i}
+	add_lunch_combo lineage_DRG-${i}
+	add_lunch_combo mk_DRG-${i}
+	add_lunch_combo nitrogen_DRG-${i}
+	add_lunch_combo rr_DRG-${i}
+	add_lunch_combo syberia_DRG-${i}
+	add_lunch_combo viper_DRG-${i}
 done
