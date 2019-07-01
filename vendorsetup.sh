13aa77abaad7366ec6c134b89733c6c54452c189
SahilSonar <sss.sonar2003@outlook.com>
2019-07-01 10:13:30 +0000

    Initial Bring-up && Clean-up!
    Signed-off-by: SahilSonar <sss.sonar2003@outlook.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,24 @@
-for i in eng userdebug user; do
-	add_lunch_combo aosp_DRG-${i}
-	add_lunch_combo aosip_DRG-${i}
-        add_lunch_combo descendant_DRG-${i}
-	add_lunch_combo dot_DRG-${i}
-	add_lunch_combo havoc_DRG-${i}
-	add_lunch_combo lineage_DRG-${i}
-	add_lunch_combo mk_DRG-${i}
-	add_lunch_combo nitrogen_DRG-${i}
-	add_lunch_combo rr_DRG-${i}
-	add_lunch_combo syberia_DRG-${i}
-	add_lunch_combo superior_DRG-${i}
-	add_lunch_combo du_DRG-${i}
-	add_lunch_combo viper_DRG-${i}
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file is executed by build/envsetup.sh, and can use anything
+# defined in envsetup.sh.
+#
+# In particular, you can add lunch options with the add_lunch_combo
+# function: add_lunch_combo generic-eng
+
+for var in eng user userdebug; do
+  add_lunch_combo lineage_DRG-$var
 done
